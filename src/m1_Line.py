8964554c4ebbeb34ff15e4d8d2de45b2eb29aac7
Liz Stutz <stutzej@rose-hulman.edu>
2019-03-11 10:29:08 -0400

    completed all todos

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -718,7 +718,8 @@ class Line(object):
             print(line2)  # Should print: Line[(0, 1), (10, 20)]
         """
         # ---------------------------------------------------------------------
-        # DONE: 13.
+        # DONE
+        # : 13.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
